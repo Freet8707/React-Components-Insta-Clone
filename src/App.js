44494604eb681ea5f4a98bff611e8025df0d7d65
Jeremy Freetly <freet8707@gmail.com>
2020-04-13 17:22:20 -0500

    project commenced with sample h1 in app.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 
   return (
     <div className="App">
-      {/* Add imported components here to render them */}
+      {/* Add imported components here to render them */}<h1>Hey Guys</h1>
     </div>
   );
 };
